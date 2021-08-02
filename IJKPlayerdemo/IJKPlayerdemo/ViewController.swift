24f34323e8a639b145233151abbc27da0ae8951d
Riven_3 <354223560@qq.com>
2021-08-02 10:11:00 +0800

    初次提交

---
+++ IJKPlayerdemo/IJKPlayerdemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IJKPlayerdemo
+//
+//  Created by 金劲通 on 2021/8/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
